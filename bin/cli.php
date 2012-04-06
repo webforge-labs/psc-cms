b012529e543ffb009c963f04e8741145ca581b6e
Philipp Scheit <p.scheit@ps-webforge.com>
2012-04-06 11:26:11 +0200

    verbose

--- bin/cli.php
+++ bin/cli.php
@@ -2,6 +2,9 @@
 
 namespace Psc;
 
+var_dump(__DIR__);
+var_dump(glob(__DIR__.DIRECTORY_SEPARATOR.'*'));
+
 require __DIR__.DIRECTORY_SEPARATOR.'psc-cms.phar.gz';
 require __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'bootstrap.php';
 
