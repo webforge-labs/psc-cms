7e4dc3c9ee96a4ac2a7ddd0fc16f2ac759c86341
pscheit <p.scheit@ps-webforge.com>
2012-04-18 21:41:44 +0200

    prevent exception filtering (phpunit)

--- bin/package.boot.php
+++ bin/package.boot.php
@@ -397,7 +397,7 @@ class ClassAutoLoader {
     $parentLength = mb_strlen($directory);
     $paths = array();
     foreach ($iterator as $file) {
-      if ($file->getExtension() === 'php') {
+      if (mb_substr($file,0,-3) === 'php') {
         $class = mb_substr(str_replace(DIRECTORY_SEPARATOR, '\\', mb_substr($file->getRealPath(),$parentLength+1)),0,-4); // mb_strlen('.php')
         $paths[$class] = (string) $file;
       }
@@ -417,7 +417,7 @@ class ClassAutoLoader {
     foreach ($iterator as $file) {
       $fInfo = $file->getFileInfo();
       
-      if ($fInfo->getExtension() === 'php') {
+      if (mb_substr($fInfo,0,-3) === 'php') {
         $class = mb_substr(str_replace('/', '\\', mb_substr((string) $fInfo, mb_strlen($wrapped)+1)), 0,-4);
         $paths[$class] = (string) $fInfo;
       }
