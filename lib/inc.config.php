782b9f791b37c8d67f5306cac741679db2458ee5
p.scheit <p.scheit@ps-webforge.com>
2012-08-03 22:34:30 +0200

    entities config fix

--- lib/inc.config.php
+++ lib/inc.config.php
@@ -6,16 +6,11 @@ $GLOBALS['conf']['jquery']['version'] = '1.7.1';
 $GLOBALS['conf']['psc-cms']['version'] = '0.2-DEV';
 
 $GLOBALS['conf']['db']['default']['host'] = 'localhost';
-$GLOBALS['conf']['db']['default']['user'] = 'root';
-$GLOBALS['conf']['db']['default']['password'] = '';
 $GLOBALS['conf']['db']['default']['database'] = 'psc-cms';
 $GLOBALS['conf']['db']['default']['port'] = NULL;
 $GLOBALS['conf']['db']['default']['charset'] = 'utf8';
-
-if (getenv('PEGASUS_CI')) {
-  $GLOBALS['conf']['db']['default']['user'] = 'psc-cms';
-  $GLOBALS['conf']['db']['default']['password'] = 'L6W2vHEbKLjeUEr2';
-}
+$GLOBALS['conf']['db']['default']['user'] = 'psc-cms';
+$GLOBALS['conf']['db']['default']['password'] = 'L6W2vHEbKLjeUEr2';
 
 if (getenv('TRAVIS_CI') === 'true') {
   $GLOBALS['conf']['db']['default']['user'] = 'root';
@@ -25,7 +20,6 @@ if (getenv('TRAVIS_CI') === 'true') {
 $GLOBALS['conf']['db']['tests'] = $GLOBALS['conf']['db']['default'];
 $GLOBALS['conf']['db']['tests']['database'] = 'psc-cms_tests';
 
-$GLOBALS['conf']['doctrine']['entities']['namespace'] = 'Entities';
 $GLOBALS['conf']['doctrine']['entities']['names'] = array();
 
 $GLOBALS['conf']['project']['title'] = 'Psc - CMS';
