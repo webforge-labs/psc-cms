4a3b823b27275966e76bc5d92fe12f2fd7ee5284
p.scheit <p.scheit@ps-webforge.com>
2012-08-03 23:44:40 +0200

    haendlisch Image hinzufuegen

--- tests/update.schema.php
+++ tests/update.schema.php
@@ -6,7 +6,6 @@ require __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'bin'.DIRECTORY_SEP
 $em = \Psc\PSC::getProject()->getModule('Doctrine')->getEntityManager('tests');
 print \Psc\Doctrine\Helper::updateSchema(\Psc\Doctrine\Helper::FORCE, "\n", $em);
 
-
 $em = \Psc\PSC::getProject()->getModule('Doctrine')->getEntityManager('default');
 print \Psc\Doctrine\Helper::updateSchema(\Psc\Doctrine\Helper::FORCE, "\n", $em);
 
