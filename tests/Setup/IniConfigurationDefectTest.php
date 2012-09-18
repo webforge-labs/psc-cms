9e8fa3f0542e68300155d4dafb48d70f58f400b4
p.scheit <p.scheit@ps-webforge.com>
2012-09-19 00:53:39 +0200

    svn:358 ConfigurationTester

---
+++ tests/Setup/IniConfigurationDefectTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Psc\Setup;
+
+/**
+ * @group class:Psc\Setup\IniConfigurationDefect
+ */
+class IniConfigurationDefectTest extends \Psc\Code\Test\Base {
+  
+  protected $iniConfigurationDefect;
+  
+  public function setUp() {
+    $this->chainClass = 'Psc\Setup\IniConfigurationDefect';
+    parent::setUp();
+    //$this->iniConfigurationDefect = new IniConfigurationDefect();
+  }
+  
+  public function testAcceptance() {
+    $this->markTestIncomplete('Stub vom Test-Creater');
+  }
+}
+?>
