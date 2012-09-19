e4bfa4ce1b383ca32b185a903933cee3168202f6
Philipp Scheit <p.scheit@ps-webforge.com>
2012-09-19 13:34:59 +0200

    svn:359

---
+++ tests/Setup/ConfigurationDefectTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Psc\Setup;
+
+/**
+ * @group class:Psc\Setup\ConfigurationDefect
+ */
+class ConfigurationDefectTest extends \Psc\Code\Test\Base {
+  
+  protected $configurationDefect;
+  
+  public function setUp() {
+    $this->chainClass = 'Psc\Setup\ConfigurationDefect';
+    parent::setUp();
+    //$this->configurationDefect = new ConfigurationDefect();
+  }
+  
+  public function testAcceptance() {
+    $this->markTestIncomplete('Stub vom Test-Creater');
+  }
+}
+?>
---
+++ tests/Setup/ConfigurationTesterTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Psc\Setup;
+
+/**
+ * @group class:Psc\Setup\ConfigurationTester
+ */
+class ConfigurationTesterTest extends \Psc\Code\Test\Base {
+  
+  protected $t;
+  
+  public function setUp() {
+    $this->chainClass = 'Psc\Setup\ConfigurationTester';
+    parent::setUp();
+    $this->t = $this->getMock('Psc\Setup\ConfigurationTester', array('retrieveIni'));
+    
+    $fakeIni = array(
+      'mbstring.internal_encoding'=>'jp18',
+      'post_max_size'=>'2M',
+      'display_errors'=>'On',
+      'register_globals'=>'0'
+    );
+    
+    $this->t->expects($this->any())->method('retrieveIni')->will($this->returnCallback(function ($name) use ($fakeIni) {
+      return $fakeIni[$name];
+    }));
+  }
+  
+  public function testWrongIniValuesAreGatheredWithinDefects() {
+    $this->t->INI('mbstring.internal_encoding', 'utf-8');
+    
+    $defects = $this->t->getDefects();
+    $this->assertCount(1, $defects);
+    $this->assertInstanceOf('Psc\Setup\IniConfigurationDefect', $iniDefect = $defects[0]);
+    $this->assertEquals('jp18', $iniDefect->getActualValue());
+    $this->assertEquals('utf-8', $iniDefect->getExpectedValue());
+  }
+  
+  public function testMegabytesGetNormalizedAndCanBeComparedToBytes() {
+    $this->t->INI('post_max_size',2*1024*1024);
+    $this->t->INI('post_max_size','2M');
+    $this->t->INI('post_max_size','2M', '>=');
+    $this->t->INI('post_max_size',1024, '<');
+    
+    $this->assertZeroDefects();
+  }
+  
+  public function testBooleansGetNormalized() {
+    $this->t->INI('display_errors', TRUE);
+    $this->t->INI('register_globals', FALSE);
+    
+    $this->assertDefects(0);
+  }
+
+  public function testBooleansGetNormalizedCmpInt() {
+    $this->t->INI('display_errors', 1);
+    $this->t->INI('register_globals', 0);
+    
+    $this->assertDefects(0);
+  }
+  
+  protected function assertZeroDefects() {
+    return $this->assertDefects(0);
+  }
+  
+  protected function assertDefects($cnt) {
+    $this->assertCount($cnt, $this->t->getDefects(), $cnt.' defects expected. Defects-List:'."\n".$this->debugDefects());
+  }
+  
+  protected function debugDefects() {
+    return \Psc\A::join($this->t->getDefects(), "%s\n");
+  }
+}
+?>
