75bfe66e7717506fb01d6485bc735aaae68064c9
p.scheit <p.scheit@ps-webforge.com>
2012-10-01 21:06:40 +0200

    svn:380 AjaxButton wird von Main mit data.request ausgelesen und blinkt dann grÃ¼n, wenn die result durchkommt

---
+++ tests/UI/AjaxButtonTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Psc\UI;
+
+/**
+ * @group class:Psc\UI\AjaxButton
+ */
+class AjaxButtonTest extends \Psc\Code\Test\Base {
+  
+  protected $ajaxButton;
+  
+  public function setUp() {
+    $this->chainClass = 'Psc\UI\AjaxButton';
+    parent::setUp();
+    //$this->ajaxButton = new AjaxButton();
+  }
+  
+  public function testAcceptance() {
+    $this->markTestIncomplete('Stub vom Test-Creater');
+  }
+}
+?>
