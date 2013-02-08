7ca32c658e0c8558c638fba19b4f630b89908cc2
Philipp Scheit <p.scheit@ps-webforge.com>
2013-02-08 15:56:35 +0100

    update container to automatically bootstrap the local package with some rude searching for the composer.json

--- lib/Psc/CMS/Container.php
+++ lib/Psc/CMS/Container.php
@@ -4,6 +4,7 @@ namespace Psc\CMS;
 
 use Webforge\Framework\Container as WebforgeContainer;
 use Webforge\Common\System\Dir;
+use Webforge\Framework\LocalPackageInitException;
 use Psc\PSC;
 
 class Container {
@@ -44,7 +45,7 @@ class Container {
   
   public function getProject() {
     if (!isset($this->project)) {
-      $this->webforge->initLocalPackageFromDirectory($this->rootDirectory);
+      $this->initLocalWebforgePackage();
       $this->project = $this->webforge->getLocalProject();
       
       if (PSC::isTravis()) {
@@ -53,6 +54,47 @@ class Container {
     }
     
     return $this->project;
+  }
+  
+  /**
+   * Tries to init the package with webforge automatically
+   *
+   * this can fail in some cases:
+   *   1. the easiest way is to webforge register-package the package which should be bootstrapped
+   *   2. the next way would be to provide a composer.json with autoload infos in the rootDirectory of the container
+   *   3. some older projects can have their composer.json in root\Umsetzung\base\src but this is discouraged to use and can be removed in the future
+   *
+   * after this is called the local package should be registered in webforge container (getLocalPackage() / getLocalProject())
+   */
+  protected function initLocalWebforgePackage() {
+    try {
+      $this->webforge->initLocalPackageFromDirectory($this->rootDirectory);
+    } catch (LocalPackageInitException $e) {
+      // this could happen for packages that are not (yet) registered by webforge
+      // but thats not a problem at first hand: we assume that we can find a composer.json somewhere
+      
+      $this->webforge->getPackageRegistry()->addComposerPackageFromDirectory(
+        $composerRoot = $this->findComposerFolder()
+      );
+      
+      // try again to init (its faster to use $composerRoot here, allthough this->rootDirectory would do it anyway)
+      $this->webforge->initLocalPackageFromDirectory($composerRoot); 
+    }  
+  }
+  
+  /**
+   * Finds the composer folder for oldstyleprojects and normal projects (webforge projects)
+   *
+   * oldStyleProjects have a "Umsetzung" Folder in its root
+   */
+  protected function findComposerFolder() {
+    $src = $this->rootDirectory->sub('Umsetzung/base/src/');
+    
+    if ($src->exists()) {
+      return $src;
+    } else {
+      return $this->rootDirectory();
+    }
   }
   
   /**
