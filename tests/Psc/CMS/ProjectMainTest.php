4d3e087bf8c2dd1eb7dff81c7f2868526605720f
Philipp Scheit <p.scheit@ps-webforge.com>
2013-03-25 10:33:41 +0100

    remove precondition

--- tests/Psc/CMS/ProjectMainTest.php
+++ tests/Psc/CMS/ProjectMainTest.php
@@ -46,10 +46,6 @@ class ProjectMainTest extends \Psc\Code\Test\Base {
   }
 
   public function testAuthControllerGetsEntityManagerInjected_Regression() {
-    // pre condition: this stupid tests uses the default entityManager
-    $this->em = $this->main->getDoctrinePackage()->getModule()->getEntityManager('tests');
-    $this->assertNotSame($this->em, $this->main->getDoctrinePackage()->getEntityManager());
-
     // set to other em for a good test
     $this->main->getDoctrinePackage()->setEntityManager($this->em);
 
