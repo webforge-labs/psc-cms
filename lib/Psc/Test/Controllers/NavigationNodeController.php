69bd6dfdac008cb6a95f3528a3d051d24e13156d
Philipp Scheit <p.scheit@ps-webforge.com>
2013-03-25 08:14:04 +0100

    make navigation controller not call parent(easier to use in tests)

--- lib/Psc/Test/Controllers/NavigationNodeController.php
+++ lib/Psc/Test/Controllers/NavigationNodeController.php
@@ -2,7 +2,19 @@
 
 namespace Psc\Test\Controllers;
 
+use Psc\Doctrine\DCPackage;
+use Psc\CMS\EntityViewPackage;
+use Psc\Form\ValidationPackage;
+use Psc\Net\ServiceErrorPackage;
+
 class NavigationNodeController extends \Psc\CMS\Controller\AbstractEntityController {
+
+  public function __construct(DCPackage $dc = NULL, EntityViewPackage $ev = NULL, ValidationPackage $v = NULL, ServiceErrorPackage $err = NULL) {
+    $this->dc = $dc;
+    $this->ev = $ev;
+    $this->v = $v;
+    $this->err = $err;
+  }
   
   public function getEntityName() {
     return 'Psc\Entities\NavigationNode';
