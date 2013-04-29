60f4c0a710aa98472b0f6e0c444a22d07ddcb29e
Philipp Scheit <p.scheit@ps-webforge.com>
2013-04-29 11:32:20 +0200

    translate ConfigMissingVariableException

--- lib/Psc/ConfigMissingVariableException.php
+++ lib/Psc/ConfigMissingVariableException.php
@@ -7,24 +7,28 @@ class ConfigMissingVariableException extends \Psc\Exception {
   public $phpCode;
   
   public $keys;
+
+  public $default;
   
   public function __construct ($message = "", $code = 0, \Exception $previous = NULL) {
     if (is_array($message)) {
       $this->keys = $message;
-      
-      $message = sprintf("Config-Variable: '%s' (default: ) nicht gefunden",
-                         implode('.',$this->keys));
-      $this->phpCode = '$GLOBALS[\'conf\']'.A::join($this->keys, "['%s']").' = NULL;';
+     
+      $ccode = '$conf'.A::join($this->keys, "['%s']");
+      $message = sprintf(
+        "Cannot read variable: '%s' from config. Put %s to your config.php",
+        implode('.',$this->keys),
+        $ccode
+      );
+
+      $this->phpCode = $ccode.' = NULL;';
     }
       
-    parent::__construct($message,$code,$previous);
+    parent::__construct($message, $code, $previous);
   }
   
   public function setDefault($default) {
-    $this->message = str_replace('(default: )','(default: '.var_export($default,TRUE).';)',$this->message);
+    $this->default = $default;
     $this->phpCode = str_replace(' = NULL;',' = '.var_export($default,TRUE).';',$this->phpCode);
   }
-  
 }
-
-?>
