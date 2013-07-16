f2c7f7cf5384caaf29997d9906084f4e46595b82
Philipp Scheit <p.scheit@ps-webforge.com>
2013-07-16 09:31:24 +0200

    allow requirejs manager to have the main data attribute configured

--- lib/Psc/JS/RequirejsManager.php
+++ lib/Psc/JS/RequirejsManager.php
@@ -5,16 +5,18 @@ namespace Psc\JS;
 class RequirejsManager extends \Psc\JS\Manager implements \Psc\HTML\HTMLInterface {
   
   protected $configuration;
+  protected $main;
   
-  public function __construct($name = 'requirejs') {
+  public function __construct($name = 'requirejs', $main = '/js/boot.js') {
     parent::__construct($name);
+    $this->main = $main;
   }
   
   public function getHTML() {
     $html = array();
     
     $html[] = Helper::load('/psc-cms-js/vendor/require.js')
-                ->setAttribute('data-main', '/js/boot.js')
+                ->setAttribute('data-main', $this->main)
                 ->setOption('br.closeTag',FALSE);
     
     foreach ($this->enqueued as $alias) {
