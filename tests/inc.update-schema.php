532d59dbd413548cc09a28f29a7e86fdc9942fb4
Philipp Scheit <p.scheit@ps-webforge.com>
2013-10-08 17:28:32 +0200

    use container to retrieve Doctrine in travis script

--- tests/inc.update-schema.php
+++ tests/inc.update-schema.php
@@ -1,12 +1,10 @@
 <?php
 
-use Psc\PSC;
 use Psc\Doctrine\Helper as DoctrineHelper;
 
 require __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'bootstrap.php';
 
-$doctrine = PSC::getProject()->getModule('Doctrine');
+$doctrine = $container->getModule('Doctrine');
 
 print DoctrineHelper::updateSchema(DoctrineHelper::FORCE, "\n", $doctrine->getEntityManager('tests'));
 print DoctrineHelper::updateSchema(DoctrineHelper::FORCE, "\n", $doctrine->getEntityManager('default'));
-?>
