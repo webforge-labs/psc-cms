c3b720ccf24775bed2ee3d73f101678891e60c4c
Philipp Scheit <p.scheit@ps-webforge.com>
2013-10-13 10:31:58 +0200

    fix path of licence notes

--- lib/Psc/DateTime/DateTime.php
+++ lib/Psc/DateTime/DateTime.php
@@ -3,5 +3,12 @@
 namespace Psc\DateTime;
 
 class DateTime extends \Webforge\Common\DateTime\DateTime implements \Psc\Data\Walkable, \Psc\Code\Info, \Psc\Data\Exportable {
+  
+  public function getWalkableType($field) {
+    if ($field === 'date') {
+      return \Psc\Data\Type\Type::create('Integer');
+    } elseif ($field === 'timezone') {
+      return \Psc\Data\Type\Type::create('String');
+    }
+  }
 }
-?>
