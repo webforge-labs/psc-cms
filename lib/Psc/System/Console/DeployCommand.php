33c502fcf2da42b4a2aef8f2c03aa438bd962bb6
Philipp Scheit <p.scheit@ps-webforge.com>
2014-07-08 13:18:33 +0200

    fix order of composer install command

--- lib/Psc/System/Console/DeployCommand.php
+++ lib/Psc/System/Console/DeployCommand.php
@@ -176,7 +176,7 @@ abstract class DeployCommand extends Command {
   protected function remoteUpdateComposer($mode, $project) {
     $this->out('[DeployCommand] ** remote Install Composer');
     if ($mode === 'staging' || $this->confirm('Do you want to install with composer?')) {
-      $cmd = 'export COMPOSER_ROOT_VERSION=dev-master; composer --optimize-autoloader --dev %s';
+      $cmd = 'export COMPOSER_ROOT_VERSION=dev-master; composer %s --optimize-autoloader --dev';
       $src = '.';
       
       $install = $this->remoteExec(
